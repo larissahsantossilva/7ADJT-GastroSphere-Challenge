5d4050e01f9b5abf6cbd9384862818bf644929ae
~ Gui Correia <103298767+CorreiaGui@users.noreply.github.com>
2025-01-04 20:23:38 -0300

    fix: padronizando logs

--- src/main/java/br/com/fiap/gastrosphere/controllers/UserController.java
+++ src/main/java/br/com/fiap/gastrosphere/controllers/UserController.java
@@ -11,16 +11,20 @@ import org.slf4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.gastrosphere.entities.User;
 import br.com.fiap.gastrosphere.services.UserService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping(UserController.V1_USER)
 public class UserController {
+
+	public static final String V1_USER = "/api/v1/users";
 
 	private static final Logger logger = getLogger(UserController.class);
 
@@ -30,18 +34,30 @@ public class UserController {
 		this.userService = userService;
 	}
 
-	// Exemplo: http://localhost:8080/users?page=1&size=10
+	// Exemplo: http://localhost:8080/api/v1/users?page=1&size=10
 	@GetMapping
-	public ResponseEntity<List<User>> findAllUsers(@RequestParam("page") int page, @RequestParam("size") int size) {
-		logger.info("Foi acessado o endpoint /users");
+	public ResponseEntity<List<User>> findAllUsers(
+			@RequestParam(value = "page", defaultValue = "1") int page, 
+			@RequestParam(value = "size", defaultValue = "10") int size) {
+		logger.info("GET | {} | Iniciado findAllUsers", V1_USER);
 		var users = this.userService.findAllUsers(page, size);
+		logger.info("GET | {} | Finalizado findAllUsers", V1_USER);
 		return ok(users);
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Optional<User>> findUserById(@PathVariable("id") UUID id) {
-		logger.info("Iniciando request | findUserById id: " + id);
+		logger.info("GET | {} | Iniciado findUserById | id: {}", V1_USER, id);
 		var user = userService.findById(id);
+		logger.info("GET | {} | Finalizado findUserByUd | id: {}", V1_USER, id);
+		return ok(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> saveUser(@RequestBody User user) {
+		logger.info("POST | {} | Iniciado saveUser | Documento: {}", V1_USER, user.getCpf());
+//		var user = userService.findById(id);
+		logger.info("POST | {} | Finalizado saveUser | Documento: {} | id: {}", V1_USER, user.getCpf(), user.getId());
 		return ok(user);
 	}
 
